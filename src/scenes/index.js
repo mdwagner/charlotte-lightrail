18a24f40255a11fa2cb5c65a4f0a3a0b8a646f9d
Michael Wagner <mdwranger@gmail.com>
2017-07-14 20:48:27 -0400

    started split up of index.ios.js for android upgrade

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -2,3 +2,4 @@ export { FAQ } from './FAQ';
 export { RailMap } from './RailMap';
 export { ScheduleInfo } from './ScheduleInfo';
 export { StationDetail } from './StationDetail';
+export { App } from './App';
