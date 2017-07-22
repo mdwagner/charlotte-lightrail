a063e92972d38b6679b26df123eab312f3d58f5c
Michael Wagner <mdwranger@gmail.com>
2017-07-22 01:56:22 -0400

    linked native modules

--- android/app/src/main/java/com/lightrail/MainApplication.java
+++ android/app/src/main/java/com/lightrail/MainApplication.java
@@ -3,6 +3,8 @@ package com.lightrail;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactNativeQuickActions.AppShortcutsPackage;
+import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new AppShortcutsPackage(),
+            new ReactNativeMapboxGLPackage()
       );
     }
   };
